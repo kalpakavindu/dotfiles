c1a93fc90a474aa9d9110fff48b2de72119af933
kalpakavindu <kalpadevonline@gmail.com>
2024-08-18 20:28:45 +0530

    update(awesomewm): ui theme icons changed

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -30,7 +30,7 @@ theme.notification_bg = "(0,0,0)"
 theme.notification_fg = "#d4be98"
 
 theme.taglist_squares_sel = themes_path .. "default/taglist/squire_focus.png"
-theme.taglist_squares_unsel = themes_path .. "default/taglist/squire_normal.png"
+theme.taglist_squares_unsel = themes_path .. "default/taglist/squire_normal_active.png"
 
 theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(15)
@@ -64,24 +64,24 @@ theme.titlebar_maximized_button_focus_active = themes_path .. "default/titlebar/
 
 theme.wallpaper = themes_path .. "default/background.jpg"
 
-theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
-theme.layout_fairv = themes_path .. "default/layouts/fairvw.png"
-theme.layout_floating = themes_path .. "default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path .. "default/layouts/magnifierw.png"
-theme.layout_max = themes_path .. "default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path .. "default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path .. "default/layouts/tilebottomw.png"
-theme.layout_tileleft = themes_path .. "default/layouts/tileleftw.png"
-theme.layout_tile = themes_path .. "default/layouts/tilew.png"
-theme.layout_tiletop = themes_path .. "default/layouts/tiletopw.png"
-theme.layout_spiral = themes_path .. "default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path .. "default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path .. "default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path .. "default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path .. "default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path .. "default/layouts/cornersew.png"
+theme.layout_fairh = themes_path .. "default/layouts/fairh.png"
+theme.layout_fairv = themes_path .. "default/layouts/fairv.png"
+theme.layout_floating = themes_path .. "default/layouts/floating.png"
+theme.layout_magnifier = themes_path .. "default/layouts/magnifier.png"
+theme.layout_max = themes_path .. "default/layouts/max.png"
+theme.layout_fullscreen = themes_path .. "default/layouts/fullscreen.png"
+theme.layout_tilebottom = themes_path .. "default/layouts/tilebottom.png"
+theme.layout_tileleft = themes_path .. "default/layouts/tileleft.png"
+theme.layout_tile = themes_path .. "default/layouts/tile.png"
+theme.layout_tiletop = themes_path .. "default/layouts/tiletop.png"
+theme.layout_spiral = themes_path .. "default/layouts/spiral.png"
+theme.layout_dwindle = themes_path .. "default/layouts/dwindle.png"
+theme.layout_cornernw = themes_path .. "default/layouts/cornernw.png"
+theme.layout_cornerne = themes_path .. "default/layouts/cornerne.png"
+theme.layout_cornersw = themes_path .. "default/layouts/cornersw.png"
+theme.layout_cornerse = themes_path .. "default/layouts/cornerse.png"
 
-theme.awesome_icon = themes_path .. "default/awesome.png"
+theme.awesome_icon = themes_path .. "default/awesome_icon.png"
 
 theme.icon_theme = nil
 
