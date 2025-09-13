7751d6e71df6a7772d56e6fdbcb008c88556d9a7
kalpakavindu <kalpadevonline@gmail.com>
2025-09-14 01:37:41 +0530

    Updated swaync stylings.

--- config/swaync/launch.sh
+++ config/swaync/launch.sh
@@ -5,12 +5,11 @@ SWAYNC_CACHE_DIR="$XDG_CACHE_HOME/swaync"
 SWAYNC_STYLE_FILE="$SWAYNC_CACHE_DIR/style.css"
 SWAYNC_CONFIG_FILE="$XDG_CONFIG_HOME/swaync/config.json"
 
-if [[ -d "$SWAYNC_CACHE_DIR" ]];then
-  sassc $XDG_CONFIG_HOME/swaync/style.scss $SWAYNC_CACHE_DIR/style.css
-else
+if [[ ! -d "$SWAYNC_CACHE_DIR" ]];then
   mkdir $SWAYNC_CACHE_DIR
-  sassc $XDG_CONFIG_HOME/swaync/style.scss $SWAYNC_CACHE_DIR/style.css
 fi
+
+sassc $XDG_CONFIG_HOME/swaync/style.scss $SWAYNC_CACHE_DIR/style.css
 
 # Uncommenting following line will open inspector
 # export GTK_DEBUG="interactive"
